b2c3397943b6fa32ed3ff9ff7e546c69135427c4
InfiniteStrike <dravenlewis.is@outlook.com>
2015-01-29 11:18:45 -0600

    updte index

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
-// this is for all the constantly selected files.
-    
-    
-
-
-
-
-})
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scroll_box').fadeIn();
+		} else {
+			$('.scroll_box').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scroll_box').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
