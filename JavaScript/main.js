41719eccb36b455fdcda626191019f38705e63e0
InfiniteStrike <dravenlewis.is@outlook.com>
2015-01-29 14:31:33 -0600

    push to block site

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -14,5 +14,35 @@ $(document).ready(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
+    
+    // cheki if using IE
+    
+      function getInternetExplorerVersion(){
+  var rv = -1;
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  else if (navigator.appName == 'Netscape')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+
+if(getInternetExplorerVersion() !== -1){
+   window.location = "http://infinitestrikeltd.github.io/unsupported.html";
+   
+}
+    
+    
+    
+    
 	
 });
