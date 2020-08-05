ac2c720225886206ca323f54ca0201b7e3ec7693
dravenlewis <dravenlewis.is@outlook.com>
2020-08-05 01:33:40 -0500

    Fixed Hire Page issues

--- pages/hire/hire.js
+++ pages/hire/hire.js
@@ -8,8 +8,11 @@ $(document).ready(() => {
                 var id = "page-" + Math.floor((Math.random() * 100));
                 protoType.addClass("page-link");
                 protoType.attr("id",id);
-                protoType.append("<h3><a href = 'http://dravenlewis.github.io/pages/showcase/"+pages[i].name+".html'>"+pages[i].title+"<a/></h3>");
-                protoType.append("<p>"+pages[i].desc+"</p>");
+                var inner = $("<div></div>");
+                inner.addClass("page-link-inner");
+                inner.append("<h3><a href = 'http://dravenlewis.github.io/pages/showcase/"+pages[i].name+".html'>"+pages[i].title+"<a/></h3>");
+                inner.append("<p>"+pages[i].desc+"</p>");
+                protoType.append(inner);
                 $(".content-main").append(protoType);
                 applyColorSelector("#" + id, color_combos[Math.floor((Math.random() * color_combos.length + 1))]);
             }
