a69038f8d6ad4b193b469baab7401fcffd697008
dravenlewis <dravenlewis.is@outlook.com>
2020-08-05 04:08:37 -0500

    Added Small Business Page

---
+++ pages/showcase/small-business/control.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+    if(isMobile()){
+        $(".is-main-view").css({"margin-left" : "0px"});
+        $(".is-side-bar").css({"width" : "0px"});
+
+        $(".is-nav-button").click(() => {
+            $(".is-main-view").animate(
+                {"margin-left" : "100%"},
+            200);
+            $(".is-side-bar").animate(
+                {"width" : "100%"},
+            200);
+            $(".is-side-bar").show();
+        });
+
+        $(".is-back-button").click(() => {
+            $(".is-main-view").animate(
+                {"margin-left" : "0%"},
+            200);
+            $(".is-side-bar").animate(
+                {"width" : "0%"},
+            200);
+            $(".is-side-bar").show();
+        });
+    }else{
+        $(".is-main-view").css({"margin-left" : "15%"});
+        $(".is-side-bar").css({"width" : "15%"});
+    }
+});
+
+function isMobile(){
+    if (window.matchMedia("only screen and (max-width: 760px)").matches) {
+        return true;
+    }else{
+        return false;
+    }
+}
