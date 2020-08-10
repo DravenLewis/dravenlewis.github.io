e086486dbe7ce5595b43238a10055a2d431ce692
dravenlewis <dravenlewis.is@outlook.com>
2020-08-09 21:47:23 -0500

    Added Personal Website Showcase

---
+++ pages/showcase/personal-website/website.js
@@ -0,0 +1,65 @@
+
+$(document).ready(() => {
+
+    setTimeout(() => {
+        $(".preloader").fadeToggle("slow", "linear");
+    }, 1000);
+
+    makeMobileChanges();
+
+    $(".is-widget-bubble").click(() => {
+        $(".is-widget-bubble-menu").slideToggle(100);
+    });
+    $(".is-dismiss").click(() => {
+        $(".is-widget-bubble-menu").slideToggle(100);
+    });
+});
+
+
+function makeMobileChanges() {
+
+    $(".hamburger").hide();
+    $(".back").hide();
+
+
+    if (isMobile()) {
+
+        $(".hamburger").show();
+        $(".back").show();
+
+
+        $(".main-content").css({ "margin-left": "0px" });
+        $(".w3-sidebar").css({ "width": "0px" });
+
+        $(".hamburger").click(() => {
+            $(".main-content").animate(
+                { "margin-left": "100%" },
+                200);
+            $(".w3-sidebar").animate(
+                { "width": "100%" },
+                200);
+            $(".w3-sidebar").show();
+        });
+
+        $(".back").click(() => {
+            $(".main-content").animate(
+                { "margin-left": "0%" },
+                200);
+            $(".w3-sidebar").animate(
+                { "width": "0%" },
+                200);
+            $(".w3-sidebar").show();
+        });
+    } else {
+        $(".main-content").css({ "margin-left": "20%" });
+        $(".w3-sidebar").css({ "width": "20%" });
+    }
+}
+
+function isMobile() {
+    if (window.matchMedia("only screen and (max-width: 760px)").matches) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- pages/showcase/small-business/control.js
+++ pages/showcase/small-business/control.js
@@ -1,48 +1,47 @@
 $(document).ready(() => {
-   makeMobileChanges();
-   
-   new ISNotification("#e74c3c","This is not a real site. This is a demo.",FOREVER,BOTTOM,true).show();
-
-   setTimeout(() => {
-    $(".preloader").fadeToggle( "slow", "linear" );
-   }, 1000);
+    setTimeout(() => {
+        $(".preloader").fadeToggle("slow", "linear");
+    }, 1000);
+    makeMobileChanges();
+    new ISNotification("#e74c3c", "This is not a real site. This is a demo.", FOREVER, BOTTOM, true).show()
 });
 
-function makeMobileChanges(){
-    if(isMobile()){
-        $(".is-main-view").css({"margin-left" : "0px"});
-        $(".is-side-bar").css({"width" : "0px"});
+function makeMobileChanges() {
+    if (isMobile()) {
+
+        $(".is-main-view").css({ "margin-left": "0px" });
+        $(".is-side-bar").css({ "width": "0px" });
 
         $(".is-nav-button").click(() => {
             $(".is-main-view").animate(
-                {"margin-left" : "100%"},
-            200);
+                { "margin-left": "100%" },
+                200);
             $(".is-side-bar").animate(
-                {"width" : "100%"},
-            200);
+                { "width": "100%" },
+                200);
             $(".is-side-bar").show();
         });
 
         $(".is-back-button").click(() => {
             $(".is-main-view").animate(
-                {"margin-left" : "0%"},
-            200);
+                { "margin-left": "0%" },
+                200);
             $(".is-side-bar").animate(
-                {"width" : "0%"},
-            200);
+                { "width": "0%" },
+                200);
             $(".is-side-bar").show();
         });
-    }else{
-        $(".is-main-view").css({"margin-left" : "20%"});
-        $(".is-side-bar").css({"width" : "20%"});
-        $(".call-to-action").css({"font-size" : "2px !important"});
+    } else {
+        $(".is-main-view").css({ "margin-left": "20%" });
+        $(".is-side-bar").css({ "width": "20%" });
+        $(".call-to-action").css({ "font-size": "2px !important" });
     }
 }
 
-function isMobile(){
+function isMobile() {
     if (window.matchMedia("only screen and (max-width: 760px)").matches) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
